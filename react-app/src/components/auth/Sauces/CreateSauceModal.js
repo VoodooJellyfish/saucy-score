069e752ab2f8250a96fffa8a17e8f53a43edb91e
tannerpedretti <tannerpedretti@gmail.com>
2021-09-22 19:39:22 -0400

    have one modal working for submitting a sauce

---
+++ react-app/src/components/auth/Sauces/CreateSauceModal.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Modal } from "../../../context/Modal";
+import CreateSauceForm from "./SauceForm";
+
+// const PostButtonStyle = styled.div`
+// .styled-button{
+//     font-size: 14px;
+//     margin: 3%;
+//     border-radius: 50%;
+//     font-weight: bold;
+//     color: rgb(191, 191, 191);
+//     padding:  10%;
+//     outline: none;
+//     border: none;
+//     background-color: #7bc2b196;
+// }
+// .styled-button:hover{
+//     background-color: #689e9296 ;
+//     transform: scale(1.01);
+// }
+
+
+// `
+
+export default function CreateSauceFormModal() {
+    const [showModal, setShowModal] = useState(false)
+
+    const handleClick = () => {
+        setShowModal(true)
+    }
+
+    const closeModal = () => {
+        setShowModal(false)
+    }
+
+
+    return (
+        <>
+            <button className="formRequestButtons" onClick={handleClick}> Submit Sauce
+            </button>
+            {showModal && <Modal onClose={() => setShowModal(false)}>
+                <CreateSauceForm closeModal={closeModal} />
+            </Modal>}
+        </>
+    )
+}
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ModalProvider } from './context/Modal';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import * as sessionActions from './store/session'
+import * as sauceActions from './store/sauce'
 
 const store = configureStore();
+
+window.store = store
+window.sauceActions = sauceActions
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+        <ModalProvider>
+            <App />
+        </ModalProvider>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
